19c23b59db4b27fd456e9c5cb77248250503f893
Isc740 <isaazcantillo@gmail.com>
2024-11-14 10:20:56 -0500

    add choice for searching one or multiple pokemon

--- js/index.js
+++ js/index.js
@@ -5,12 +5,19 @@ async function fetchPokemonData(pokemon) {
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-    let times = prompt("Ingrese el numero de pokemones que quiere ver")
-    for (let i = 1; i <= times; i++) {
-        const pokemonData = await fetchPokemonData(i);
-        document.querySelector(".container").innerHTML += addPokemonComponent(pokemonData);
+    let question = Number(prompt("Quieres (1) buscar, o (2) mostrar un rango de pokemones?"));
+    if (question === 1) {
+        const pokemonName = prompt("Inserte el nombre del pokemon")
+        const pokemonData = await fetchPokemonData(pokemonName);
+        document.querySelector(".container").innerHTML = addPokemonComponent(pokemonData);
+    } else {
+        let start = prompt("Ingrese el numero de donde empezar");
+        let end = prompt("Ingrese el numero del final");
+        for (let i = start; i <= end; i++) {
+            const pokemonData = await fetchPokemonData(i);
+            document.querySelector(".container").innerHTML += addPokemonComponent(pokemonData);
+        }
     }
-
 })
 
 const addPokemonComponent = (pokemonData) => `
