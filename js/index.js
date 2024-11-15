1ef06af02719892e7688c2252fae9c16a2f07f73
Isc740 <isaazcantillo@gmail.com>
2024-11-15 08:29:50 -0500

    enable strict mode

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 async function fetchPokemonData(pokemon) {
 	const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 	const data = await response.json();
@@ -9,11 +11,22 @@ async function displayMultiplePokemons(start, end) {
 	for (let i = start; i <= end; i++) {
 		promises.push(fetchPokemonData(i));
 	}
+
 	let pokemonDataArray = await Promise.all(promises);
 	document.querySelector(".container").innerHTML = pokemonDataArray
 		.map((pokemonData) => addPokemonComponent(pokemonData))
 		.join("");
 }
+
+const addPokemonComponent = (pokemonData) => `
+  <div class="card" style="width: 24rem;">
+     <img class="card-img-top img-fluid" src="${pokemonData.sprites.other["official-artwork"].front_default}" alt="Card image cap">
+     <div class="card-body">
+         <h5 class="card-title">${pokemonData.species.name}</h5>
+         <p class="card-text">Pokemon numero: <strong>${pokemonData.id}</strong></p>
+         <a href="#" class="btn btn-primary">Ver pokemon</a>
+     </div>
+    </div>`;
 
 document.addEventListener("DOMContentLoaded", async () => {
 	let question = Number(
@@ -31,13 +44,3 @@ document.addEventListener("DOMContentLoaded", async () => {
 			addPokemonComponent(pokemonData);
 	}
 });
-
-const addPokemonComponent = (pokemonData) => `
-  <div class="card" style="width: 24rem;">
-     <img class="card-img-top img-fluid" src="${pokemonData.sprites.other["official-artwork"].front_default}" alt="Card image cap">
-     <div class="card-body">
-         <h5 class="card-title">${pokemonData.species.name}</h5>
-         <p class="card-text">Pokemon numero: <strong>${pokemonData.id}</strong></p>
-         <a href="#" class="btn btn-primary">Ver pokemon</a>
-     </div>
-    </div>`;
